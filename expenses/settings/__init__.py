01d7f3da40c41b7575405d65502840c07e7a6862
zamizmi <sami.lindqvist@helsinki.fi>
2020-02-11 16:26:30 +0000

    docker updates

--- expenses/settings/__init__.py
+++ expenses/settings/__init__.py
@@ -12,7 +12,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__).replace('s
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -38,7 +38,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['kululasku.partio.fi','localhost']
+ALLOWED_HOSTS = ['partio-kulu.perfekt.io','localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
