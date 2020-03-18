e5548dc83c8362df488a6d5bb63d25406965f2de
sami lindqvist <sami.k.lindqvist@gmail.com>
2020-03-18 17:48:16 +0200

    fix typo

--- expenses/apps/expenseapp/management/commands/send_katre.py
+++ expenses/apps/expenseapp/management/commands/send_katre.py
@@ -27,7 +27,7 @@ class Command (BaseCommand):
       import zipfile, io
 
       date_str = datetime.now().strftime("%Y-%m-%d-%H-%M")
-      o = os.path.join('/code/tmp/zips', 'katre_' + 'date_str' + '.zip')
+      o = os.path.join('/code/tmp/zips', 'katre_' + date_str + '.zip')
       h = os.path.join('/code/tmp/zips', 'katre_hapa_' + date_str + '.zip')
       
       self.stdout.write('Creating katre package %s' % o)
