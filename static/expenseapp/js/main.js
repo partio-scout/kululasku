04c66621e8f21d68a79d2fb310cf05acc59ed3f8
sami lindqvist <sami.k.lindqvist@gmail.com>
2020-06-11 11:15:12 +0300

    fix form handling bug: date fields where not shown

--- static/expenseapp/js/main.js
+++ static/expenseapp/js/main.js
@@ -1,7 +1,104 @@
 $(function() {
+  /* Calculate whole form total */
+  updateTotal = function UpdateTotal() {
+    var $total = $('#expense-total span'),
+        sum = 0.0;
+    $('#expenses .expenseline').each(function() {
+      sum += parseFloat($(this).data('subtotal'));
+    });
+    
+    $total.text(sum.toFixed(2).replace('.', ',') + ' €');
+  }
+
+  /* Function to find the chosen expensetype from the JSON calculated in the beginning. */
+  var findExpensetypeData = function findExpensetypeData(seekFor) {
+    var correct;
+    $.each(expensetype_data, function(i, el) {
+      if (el.name == seekFor) {
+        correct = el;
+        return false;
+      }
+    });
+    return correct;
+   }
+
+  /* Function to update the given row to match the current state. Called in the beginning and on change for the row. */
+  updateRow = function updateRow($row) {
+    if(!$row.find('.subtotal').length) {
+      $row.find('input[id$=basis]').after($('<span class="subtotal"/>'));
+    }
+    
+    var $type = $row.find('[id$="expensetype"] :selected'),
+        $subtotal = $row.find('.subtotal'),
+        row_sum = 0;
+        
+    if($type.val() != '') {
+      var $type_data = findExpensetypeData($type.text()),
+          $basis = $row.find('input[id$=basis]'),
+          subtotal_text = '';
+      if($type_data) {
+        subtotal_text = ' ' + $type_data.unit + ' × ' + $type_data.multiplier.toString().replace('.', ',') + ' = ';
+
+        if($type_data.requires_endtime) {
+          $row.find(".ended_at_date").parent().addClass('required');
+          $row.find(".ended_at_date").parent().removeClass('visuallyhidden')
+          $row.find(".ended_at_date").prop("disabled", false)
+  
+          $row.find(".ended_at_time").parent().addClass('required');
+          $row.find(".ended_at_time").parent().removeClass('visuallyhidden')
+          $row.find(".ended_at_time").prop("disabled", false)
+        } else {
+          $row.find(".ended_at_date").parent().addClass('required');
+          $row.find(".ended_at_date").parent().addClass('visuallyhidden')
+          $row.find(".ended_at_time").prop("disabled", true)
+  
+          $row.find(".ended_at_time").parent().removeClass('required');
+          $row.find(".ended_at_time").parent().addClass('visuallyhidden')
+          $row.find(".ended_at_time").prop("disabled", true)
+        }
+  
+        if($type_data.requires_start_time) {
+          $row.find(".begin_at_time").parent().addClass('required');
+          $row.find(".begin_at_time").parent().removeClass('visuallyhidden')
+          $row.find(".begin_at_time").prop("disabled", false)
+        } else {
+          $row.find(".begin_at_time").parent().removeClass('required');
+          $row.find(".begin_at_time").parent().addClass('visuallyhidden')
+          $row.find(".begin_at_time").prop("disabled", true)
+        }
+        
+       if($basis.val() != '') {
+          var basis = parseFloat($basis.val().replace(',', '.'));
+          if (basis > 0) {
+            var sum = basis * $type_data.multiplier;
+            row_sum = sum.toFixed(2)
+            subtotal_text += row_sum.replace('.', ',') + ' €';
+          }
+        }
+      }
+      
+      $subtotal.text(subtotal_text);
+    } else {
+      $subtotal.text('');
+    }
+    
+    $row.data('subtotal', row_sum);
+    updateTotal();
+  }
   
   /* Create new expense -form */
   if($('#expenses').length) {
+    /* Calculate single row total */
+    // Find this once
+  if($('[id$="expensetype_data"]'.length>0)) {
+    var expensetype_data = JSON.parse($('[id$="expensetype_data"]').first().val());
+    $.each(expensetype_data, function() {
+      this.multiplier = parseFloat(this.multiplier);
+    });
+  } 
+    $(this).find('.expenseline').each(function() {
+      updateRow($(this));
+    })
 
     if($('.delete-expenseline').length > 1) {
       $('.delete-expenseline').css('background-color','#253764');
@@ -9,13 +106,7 @@ $(function() {
 
     if($('.delete-expenseline').length <= 2) {
       $('.delete-expenseline').css('background-color','grey');
-  }
-
-  /* Prevent removing the last expenseline. 1 button is hidden for copying -> length ==2 */
-    // if($('.delete-expenseline').length == 2) {
-    //   $('.delete-expenseline button').first().prop('hidden')
-    //   $('.delete-expenseline').first().prop('disabled', true)
-    // }
+    }
 
     $(this).find(".datepicker").each(function() {
       $(this).datepicker({
@@ -70,17 +161,6 @@ $(function() {
           form.parsley( 'addItem', $(this));
         });
     });
-
-    findExpensetypeData2 =function findExpensetypeData2(seekFor) {
-      var correct;
-      $.each(expensetype_data, function(i, el) {
-        if (el.name == seekFor) {
-          correct = el;
-          return false;
-        }
-      });
-      return correct;
-    }
     
     /* Delete single expenselines without removing all of them*/
     $('#expenses').on('click', '.delete-expenseline', function(e) {
@@ -99,108 +179,22 @@ $(function() {
         });
       
       // Remove the actual element
-
       line.remove();
 
       if($('.delete-expenseline').length <= 2) {
           $('.delete-expenseline').css('background-color','grey');
       }
-    });
-    
-    /* Calculate single row total */
-    // Find this once
-    var expensetype_data = JSON.parse($('[id$="expensetype_data"]').first().val());
-    $.each(expensetype_data, function() {
-      this.multiplier = parseFloat(this.multiplier);
-    });
-    
-    var findExpensetypeData = function findExpensetypeData(seekFor) {
-      var correct;
-      $.each(expensetype_data, function(i, el) {
-        if (el.name == seekFor) {
-          correct = el;
-          return false;
-        }
-      });
-      return correct;
-    }, updateRow = function updateRow($row) {
-      if(!$row.find('.subtotal').length) {
-        $row.find('input[id$=basis]').after($('<span class="subtotal"/>'));
-      }
       
-      var $type = $row.find('[id$="expensetype"] :selected'),
-          $subtotal = $row.find('.subtotal'),
-          row_sum = 0;
-          
-      if($type.val() != '') {
-        var $type_data = findExpensetypeData($type.text()),
-            $basis = $row.find('input[id$=basis]'),
-            subtotal_text = '';
-        if($type_data) {
-          subtotal_text = ' ' + $type_data.unit + ' × ' + $type_data.multiplier.toString().replace('.', ',') + ' = ';
-
-          if($type_data.requires_endtime) {
-            $row.find(".ended_at_date").parent().addClass('required');
-            $row.find(".ended_at_date").parent().removeClass('visuallyhidden')
-            $row.find(".ended_at_date").prop("disabled", false)
-    
-            $row.find(".ended_at_time").parent().addClass('required');
-            $row.find(".ended_at_time").parent().removeClass('visuallyhidden')
-            $row.find(".ended_at_time").prop("disabled", false)
-          } else {
-            $row.find(".ended_at_date").parent().addClass('required');
-            $row.find(".ended_at_date").parent().addClass('visuallyhidden')
-            $row.find(".ended_at_time").prop("disabled", true)
-    
-            $row.find(".ended_at_time").parent().removeClass('required');
-            $row.find(".ended_at_time").parent().addClass('visuallyhidden')
-            $row.find(".ended_at_time").prop("disabled", true)
-          }
-    
-          if($type_data.requires_start_time) {
-            $row.find(".begin_at_time").parent().addClass('required');
-            $row.find(".begin_at_time").parent().removeClass('visuallyhidden')
-            $row.find(".begin_at_time").prop("disabled", false)
-          } else {
-            $row.find(".begin_at_time").parent().removeClass('required');
-            $row.find(".begin_at_time").parent().addClass('visuallyhidden')
-            $row.find(".begin_at_time").prop("disabled", true)
-          }
-          
-         if($basis.val() != '') {
-            var basis = parseFloat($basis.val().replace(',', '.'));
-            if (basis > 0) {
-              var sum = basis * $type_data.multiplier;
-              row_sum = sum.toFixed(2)
-              subtotal_text += row_sum.replace('.', ',') + ' €';
-            }
-          }
-        }
-        
-        $subtotal.text(subtotal_text);
-        
-      } else {
-        $subtotal.text('');
-      }
-      
-      $row.data('subtotal', row_sum);
+      // Calculate total after removing a row.
       updateTotal();
-      
-    }, updateTotal = function UpdateTotal() {
-      var $total = $('#expense-total span'),
-          sum = 0.0;
-      $('#expenses .expenseline').each(function() {
-        sum += parseFloat($(this).data('subtotal'));
-      });
-      
-      $total.text(sum.toFixed(2).replace('.', ',') + ' €');
-    }
-    
+    });
+
+
+    // Update row data after every change.
     $('#expenses').on('change keyup', '[id$="expensetype"], [id$="basis"]', function() {
       updateRow($(this).closest('.expenseline'));
     });
         
-    /* Calculate whole form total */
     
     /* Delegate clicks to calendar icon to the actual input element */
     // $('#expenses').on('click', 'i.icon-calendar', function() {
@@ -214,7 +208,6 @@ $(function() {
     $('#expense-form').on('submit', function() {
       
       fixIndexes();
-      
       // Fix management form
       $('#id_expenseform_EXPENSELINES-TOTAL_FORMS').val($('#expenses .expenseline').length);
       
