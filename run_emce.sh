6ef4bb1c5628fa4eab62a74bf904ba944d5d52a3
Sami Lindqvist <sami.k.lindqvist@gmail.com>
2022-02-10 11:25:54 +0200

    update run_emce command

--- run_emce.sh
+++ run_emce.sh
@@ -1,9 +1,9 @@
 now=$(date)
 
 echo start sending invoices, $now
-docker-compose exec -T web bash -c "python manage.py send_invoices" >> /mnt/storage/logs/emce_logs.txt
+docker-compose -f docker-compose-prod.yml exec -T web bash -c "python manage.py send_invoices" >> /mnt/storage/logs/emce_logs.txt
 echo invoice script over
 
 echo start sending katres, $now
-docker-compose exec -T web bash -c "python manage.py send_katre" >> /mnt/storage/logs/emce_logs.txt
+docker-compose -f docker-compoes-prod.yml exec -T web bash -c "python manage.py send_katre" >> /mnt/storage/logs/emce_logs.txt
 echo katre script over
