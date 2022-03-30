1e602f8280448846243d9a3acdde6d1ff386d6fb
Sami Lindqvist <sami.k.lindqvist@gmail.com>
2022-03-30 14:55:18 +0300

    add year to log

--- run_emce.sh
+++ run_emce.sh
@@ -1,9 +1,9 @@
 now=$(date)
 
 echo start sending invoices, $now
-docker-compose -f docker-compose-prod.yml exec -T web bash -c "python manage.py send_invoices" >> /mnt/storage/logs/emce_logs.txt
+docker-compose -f docker-compose-prod.yml exec -T web bash -c "python manage.py send_invoices" >> /mnt/storage/logs/emce_logs_2022.txt
 echo invoice script over
 
 echo start sending katres, $now
-docker-compose -f docker-compose-prod.yml exec -T web bash -c "python manage.py send_katre" >> /mnt/storage/logs/emce_logs.txt
+docker-compose -f docker-compose-prod.yml exec -T web bash -c "python manage.py send_katre" >> /mnt/storage/logs/emce_logs_2022.txt
 echo katre script over
